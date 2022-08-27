6403f43d4db7459f0bc72512f063675eff32750e
Leila Moraes <leila.violino@gmail.com>
2022-08-27 00:44:12 -0300

    conclusão do requisito 13

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -16,4 +16,39 @@ describe('Testes da função getOpeningHours', () => {
     expect(getOpeningHours('MONDAY', '8:00-am')).toBe(closedMessage);
     expect(getOpeningHours('MoNdAy', '8:00-aM')).toBe(closedMessage);
   });
+
+  it('Testa se a mensagem de erro das horas está sendo passada corretamente', () => {
+    function errorHour() {
+      getOpeningHours('monday', 'oito horas');
+    }
+    expect(errorHour).toThrowError(new Error('The hour should represent a number'));
+  });
+
+  it('Testa se a mensagem de erro da abraviação das horas está sendo passada corretamente', () => {
+    function errorAbbreviation() {
+      getOpeningHours('friday', '4:00-dm');
+    }
+    expect(errorAbbreviation).toThrowError(new Error('The abbreviation must be \'AM\' or \'PM\''));
+  });
+
+  it('Testa se a mensagem de erro dos número das horas está sendo passada corretamente', () => {
+    function errorHourAndMinutes() {
+      getOpeningHours('friday', '13:00-pm');
+    }
+    expect(errorHourAndMinutes).toThrowError(new Error('The hour must be between 0 and 12'));
+  });
+
+  it('Testa se a mensagem de erro do número dos minutos está sendo passada corretamente', () => {
+    function errorHourAndMinutes() {
+      getOpeningHours('friday', '12:60-pm');
+    }
+    expect(errorHourAndMinutes).toThrowError(new Error('The minutes must be between 0 and 59'));
+  });
+
+  it('Testa se a mensagem de erro dos dias da semana está sendo passada corretamente', () => {
+    function errorDay() {
+      getOpeningHours('domingo', '4:00-pm');
+    }
+    expect(errorDay).toThrowError(new Error('The day must be valid. Example: Monday'));
+  });
 });
