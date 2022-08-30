ea9e577ffbee582c0d165d243afbef88aa7fe1f4
Leila Moraes <leila.violino@gmail.com>
2022-08-30 17:31:48 -0300

    conclusão do requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const getAnimal = species.find((element) => element.name === animal);
+  return getAnimal.residents.every((element) => element.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
