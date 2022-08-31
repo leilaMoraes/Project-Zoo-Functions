3c5ea770b0f6817fb5f0e466d5877ee02ada50d6
Leila Moraes <leila.violino@gmail.com>
2022-08-31 17:28:59 -0300

    conclusão do requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const managers = employees.filter((employee) => employee.managers.includes(managerId));
+  if (managers.length === 0) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return managers.map((manager) => `${manager.firstName} ${manager.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
