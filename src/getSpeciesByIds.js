3c726ff469704a1dad3693fd384005b0e389d444
Leila Moraes <leila.violino@gmail.com>
2022-08-31 13:19:46 -0300

    conclusão do requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length > 0) {
+    const animals = [];
+    ids.forEach((element) => {
+      animals.push(data.species.find((specie) => specie.id === element));
+    });
+    return animals;
+  }
+  return [];
 }
 
 module.exports = getSpeciesByIds;
