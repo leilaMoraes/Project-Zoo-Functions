07ee2ec15f7a616e604f156fafdacd01acb71ef5
Leila Moraes <leila.violino@gmail.com>
2022-09-01 15:49:51 -0300

    conclusão do requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
+const childPrice = data.prices.child;
+const adultPrice = data.prices.adult;
+const seniorPrice = data.prices.senior;
 
+function countEntrants(entrants) {
+  return {
+    child: entrants.filter((entrant) => entrant.age < 18).length,
+    adult: entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length,
+    senior: entrants.filter((entrant) => entrant.age >= 50).length };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const child = entrants.filter((entrant) => entrant.age < 18);
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50);
+  const senior = entrants.filter((entrant) => entrant.age >= 50);
+  return child.length * childPrice + adult.length * adultPrice + senior.length * seniorPrice;
 }
 
 module.exports = { calculateEntry, countEntrants };
