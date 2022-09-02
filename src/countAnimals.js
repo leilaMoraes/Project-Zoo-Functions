cd1caf0708395a31c5d78e430f70b7f0b077096d
Leila Moraes <leila.violino@gmail.com>
2022-09-02 00:26:12 -0300

    conclusão do requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const saida = {};
+  data.species.forEach((specie) => {
+    saida[specie.name] = specie.residents.length;
+  });
+  if (animal === undefined) {
+    return saida;
+  }
+  const zooAnimal = data.species.find((element) => element.name === animal.specie);
+  if (Object.keys(animal).length === 1) {
+    const animalAmount = zooAnimal.residents.length;
+    return animalAmount;
+  } const animalSex = zooAnimal.residents.filter((resident) => resident.sex === animal.sex);
+  return animalSex.length;
 }
 
 module.exports = countAnimals;
