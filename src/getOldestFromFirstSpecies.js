1ad37ecabbfcc145be5f0e85dfee27b1f0601aef
Leila Moraes <leila.violino@gmail.com>
2022-09-07 18:02:19 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const getEmployee = data.employees.find((employee) => employee.id === id);
+  const getFirstAnimal = getEmployee.responsibleFor.find((animal) => animal);
+  const getAnimal = data.species.find((specie) => specie.id === getFirstAnimal);
+  const animalResident = getAnimal.residents.map((resident) => resident.age);
+  const olderAnimal = animalResident.reduce((acc, curr) => (curr > acc ? curr : acc));
+  const findOlderAnimal = getAnimal.residents.find((old) => old.age === olderAnimal);
+  const { name, sex, age } = findOlderAnimal;
+
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
